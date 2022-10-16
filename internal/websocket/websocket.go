3e16a756b294f0d568b7abc215216f7117590172
aboublef <kristopherab@gmail.com>
2022-10-16 18:33:04 +0100

    mid-level api for read/write(string)

--- internal/websocket/websocket.go
+++ internal/websocket/websocket.go
@@ -54,7 +54,6 @@ func (c serverConn) ReadString() (string, error) {
 }
 
 func (c serverConn) ReadJSON(v any) error {
-	// NOTE read next frame goes in here for now
 	h, err := c.r.NextFrame()
 	if err != nil {
 		return err
@@ -85,7 +84,18 @@ func (c serverConn) WriteJSON(v any) error {
 	return c.w.Flush()
 }
 
+func UpgradeHTTP(w http.ResponseWriter, r *http.Request) (conn Conn, err error) {
+	rwc, _, _, err := ws.UpgradeHTTP(r, w)
+	c := serverConn{rwc: rwc}
+	c.r = wsutil.NewReader(rwc, c.State())
+	c.w = wsutil.NewWriter(rwc, c.State(), ws.OpText)
+	return c, err
+}
+
 type clientConn struct {
+	r *wsutil.Reader
+	w *wsutil.Writer
+
 	rwc net.Conn
 }
 
@@ -94,28 +104,32 @@ func (c clientConn) State() ws.State { return ws.StateClientSide }
 func (c clientConn) Close() error { return c.rwc.Close() }
 
 func (c clientConn) Read() ([]byte, error) {
-	b, err := wsutil.ReadServerText(c.rwc)
+	b, err := wsutil.ReadServerBinary(c.rwc)
 	return b, err
 }
 
 func (c clientConn) ReadString() (string, error) {
-	p, _, err := wsutil.ReadData(c.rwc, c.State())
-	// b, err := wsutil.ReadServerText(c.rwc)
-	return string(p), err
+	h, err := c.r.NextFrame()
+	if err != nil {
+		return "", err
+	}
+
+	// Reset writer to write frame with right operation code.
+	c.w.Reset(c.rwc, c.State(), h.OpCode)
+
+	b, err := io.ReadAll(c.r)
+	return string(b), err
 }
 
 func (c clientConn) ReadJSON(v any) error {
-	r := wsutil.NewReader(c.rwc, c.State())
-
-	// NOTE read next frame goes in here for now
-	h, err := r.NextFrame()
+	h, err := c.r.NextFrame()
 	if err != nil {
 		return err
 	}
 	if h.OpCode == ws.OpClose {
 		return io.EOF
 	}
-	return json.NewDecoder(r).Decode(v)
+	return json.NewDecoder(c.r).Decode(v)
 }
 
 func (c clientConn) Write(p []byte) error {
@@ -123,30 +137,25 @@ func (c clientConn) Write(p []byte) error {
 }
 
 func (c clientConn) WriteString(s string) error {
-	return wsutil.WriteMessage(c.rwc, c.State(), ws.OpText, []byte(s))
+	// return wsutil.WriteMessage(c.rwc, c.State(), ws.OpText, []byte(s))
+	_, err := io.WriteString(c.w, s)
+	if err != nil {
+		return err
+	}
+	return c.w.Flush()
 }
 
 func (c clientConn) WriteJSON(v any) error {
-	w := wsutil.NewWriter(c.rwc, c.State(), ws.OpText)
-
-	if err := json.NewEncoder(w).Encode(v); err != nil {
+	if err := json.NewEncoder(c.w).Encode(v); err != nil {
 		return err
 	}
-	return w.Flush()
+	return c.w.Flush()
 }
 
-func UpgradeHTTP(w http.ResponseWriter, r *http.Request) (c Conn, err error) {
-	rwc, _, _, err := ws.UpgradeHTTP(r, w)
-	// TODO remove the need to define reader/write on the connection
-	return serverConn{
-		r:   wsutil.NewReader(rwc, ws.StateServerSide),
-		w:   wsutil.NewWriter(rwc, ws.StateServerSide, ws.OpText),
-		rwc: rwc,
-	}, err
-}
-
-func Dial(ctx context.Context, urlStr string) (c Conn, err error) {
+func Dial(ctx context.Context, urlStr string) (conn Conn, err error) {
 	rwc, _, _, err := ws.Dial(context.Background(), urlStr)
-	// TODO remove the need to define reader/write on the connection
-	return clientConn{rwc}, err
+	c := clientConn{rwc: rwc}
+	c.r = wsutil.NewReader(rwc, c.State())
+	c.w = wsutil.NewWriter(rwc, c.State(), ws.OpText)
+	return c, err
 }
